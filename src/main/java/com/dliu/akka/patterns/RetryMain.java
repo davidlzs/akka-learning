c2db4c584e04668d720fa39b081088c101ba2e85
David Liu <david.liu@yardi.com>
2021-01-27 14:09:11 -0600

    Demo of Akka Retry Pattern - add backoff retry

--- src/main/java/com/dliu/akka/patterns/RetryMain.java
+++ src/main/java/com/dliu/akka/patterns/RetryMain.java
@@ -1,5 +1,9 @@
 package com.dliu.akka.patterns;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
 import java.util.UUID;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
@@ -14,26 +18,40 @@ Demo of Akka Retry Pattern https://doc.akka.io/docs/akka/current/futures.html#re
 
  */
 public class RetryMain {
-
-    public static final int NUMBER_OF_FAILURES = 2;
-    public static final int ATTEMPTS = 3;
+    private static final Logger LOGGER = LoggerFactory.getLogger(RetryMain.class);
+    public static final int NUMBER_OF_FAILURES = 4;
+    public static final int ATTEMPTS = 5;
 
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("retry-system");
-        AtomicInteger attempts = new AtomicInteger(0);
-        // simulate failed future call
-        Callable<CompletionStage<String>> attempt = () -> {
-            int i = attempts.incrementAndGet();
-            System.out.println("calling... for try#: " + i);
-            if (attempts.get() < NUMBER_OF_FAILURES) {
-                throw new RuntimeException("Failed to response");
-            }
-            return CompletableFuture.completedFuture(UUID.randomUUID().toString());
-        };
 
+        Callable<CompletionStage<String>> attempt = simulateFailedAttempts();
+        CompletionStage<String> retriedFuture = fixedIntervalRetries(system, attempt);
+        executeRetries(retriedFuture);
+
+        Callable<CompletionStage<String>> backoffAttempt = simulateFailedAttempts();
+        CompletionStage<String> backoffRetriedFuture = backOffRetries(system, backoffAttempt);
+        executeRetries(backoffRetriedFuture);
+
+
+    }
+
+    private static CompletionStage<String> fixedIntervalRetries(ActorSystem system, Callable<CompletionStage<String>> attempt) {
         CompletionStage<String> retriedFuture =
                 Patterns.retry(attempt, ATTEMPTS, java.time.Duration.ofMillis(200), system.scheduler(), system.dispatcher());
+        return retriedFuture;
+    }
 
+    private static CompletionStage<String> backOffRetries(ActorSystem system, Callable<CompletionStage<String>> attempt) {
+        Duration minBackoff = Duration.ofSeconds(1);
+        Duration maxBackoff = Duration.ofSeconds(30);
+
+        CompletionStage<String> retriedFuture =
+                Patterns.retry(attempt, ATTEMPTS, minBackoff, maxBackoff, 0.2, system.scheduler(), system.dispatcher());
+        return retriedFuture;
+    }
+
+    private static void executeRetries(CompletionStage<String> retriedFuture) {
         try {
             retriedFuture
                     .handle((ack, ex) -> {
@@ -43,15 +61,28 @@ public class RetryMain {
                         }
                         else return ack;
                     }).toCompletableFuture() //.join()
-                    /*.exceptionally((ex) -> {
-                        System.err.println("failed " + ex.getMessage());
+                    .exceptionally((ex) -> {
+                        LOGGER.error("failed ", ex);
                         throw new RuntimeException("Exception happened with message " + ex.getMessage());
-                    })*/
-                    .thenAccept((s) -> System.out.println("got response " + s))
-                    //.join() // when you join or get, it will block the main thread and the exception will be print out as java.util.concurrent.CompletionExcepiton(runtimeException)
+                    })
+                    .thenAccept((s) -> LOGGER.info("got response {}", s))
+                    //.join() // when you join or get, it will block the main thread and the exception will be print out as java.util.concurrent.CompletionException(runtimeException)
                     ;
         } catch (Exception e) {
             System.err.println("got exception " + e);
         }
+    }
+
+    private static Callable<CompletionStage<String>> simulateFailedAttempts() {
+        AtomicInteger attempts = new AtomicInteger(0);
+        // simulate failed future call
+        return () -> {
+            int i = attempts.incrementAndGet();
+            LOGGER.info("Serve attempt {}", i);
+            if (attempts.get() < NUMBER_OF_FAILURES) {
+                throw new RuntimeException("Failed to response");
+            }
+            return CompletableFuture.completedFuture(UUID.randomUUID().toString());
+        };
     }
 }
