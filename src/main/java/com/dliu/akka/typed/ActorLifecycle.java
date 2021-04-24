8c811f3f06643fff0489673236ca78f0c9bda353
davidlzs <davidlzs@gmail.com>
2021-04-24 08:46:05 -0600

    Add subscribe deadletter in lifecycle example

--- src/main/java/com/dliu/akka/typed/ActorLifecycle.java
+++ src/main/java/com/dliu/akka/typed/ActorLifecycle.java
@@ -1,9 +1,8 @@
 package com.dliu.akka.typed;
 
-import akka.actor.typed.ActorSystem;
-import akka.actor.typed.Behavior;
-import akka.actor.typed.PostStop;
-import akka.actor.typed.PreRestart;
+import akka.actor.DeadLetter;
+import akka.actor.typed.*;
+import akka.actor.typed.eventstream.EventStream;
 import akka.actor.typed.javadsl.AskPattern;
 import akka.actor.typed.javadsl.Behaviors;
 
@@ -15,31 +14,28 @@ import java.util.concurrent.ExecutionException;
 public class ActorLifecycle {
 
     public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
-        ActorSystem<String> system = ActorSystem.<String>create(Behaviors.setup(ctx -> {
-
-            Behavior<String> behavior = Behaviors.receive(String.class)
+        ActorSystem<Object> system = ActorSystem.create(Behaviors.setup(ctx -> {
+            ActorRef<Object> a1 = ctx.spawn(getStringBehavior(), "a1");
+            ActorRef<Object> a2 = ctx.spawn(getStringBehavior(), "a2");
+            ctx.getLog().info("a1: {} and a2: {}", a1.path(), a2.path());
+            return Behaviors.receive(Object.class)
                     .onMessage(String.class, m -> {
-                        ctx.getLog().info("Received message: {} ", m);
-                        if (m.equals("Exception")) {
-                            throw new RuntimeException("Exception");
-                        } else if (m.equals("Stop")) {
-                            Behaviors.stopped();
-                        } else {
-                        }
+                        a1.tell(m);
+                        a2.tell(m);
                         return Behaviors.same();
                     })
-                    .onSignal(PreRestart.class, pr -> {
-                        ctx.getLog().info("PreRestart");
-                        return Behaviors.same();
-                    })
-                    .onSignal(PostStop.class, s -> {
-                        ctx.getLog().info("PostStop");
+                    .onMessage(DeadLetter.class, d -> {
+                        ctx.getLog().info("Received dead letter " + d);
                         return Behaviors.same();
                     })
                     .build();
-            return behavior;
 
         }), "lifecycle");
+
+        // How to register
+
+        system.eventStream().tell(new EventStream.Subscribe(DeadLetter.class, system));
+
 
         system.tell("Hello");
 
@@ -50,5 +46,31 @@ public class ActorLifecycle {
 
         System.in.read();
         system.terminate();
+    }
+
+    private static Behavior<Object> getStringBehavior() {
+        return Behaviors.setup(ctx -> {
+            ctx.getLog().info("ctx getSelf: {}", ctx.getSelf().path());
+            return Behaviors.receive(Object.class)
+                    .onMessage(String.class, m -> {
+                        System.out.println(ctx.getSelf().path() + " Received message: " + m);
+                        if (m.equals("Exception")) {
+                            throw new RuntimeException("Exception");
+                        } else if (m.equals("Stop")) {
+                            Behaviors.stopped();
+                        } else {
+                        }
+                        return Behaviors.same();
+                    })
+                    .onSignal(PreRestart.class, pr -> {
+                        ctx.getLog().info("PreRestart " + ctx.getSelf());
+                        return Behaviors.same();
+                    })
+                    .onSignal(PostStop.class, s -> {
+                        ctx.getLog().info("PostStop " + ctx.getSelf());
+                        return Behaviors.same();
+                    })
+                    .build();
+        });
     }
 }
