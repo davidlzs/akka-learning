44f1e132f8befad359090d49fd8938a96c62053c
David Liu <david.liu@yardi.com>
2021-08-10 08:34:17 -0600

    Akka http client POST method

--- src/main/java/com/dliu/akka/lab/http/HttpClientMain.java
+++ src/main/java/com/dliu/akka/lab/http/HttpClientMain.java
@@ -10,19 +10,35 @@ import akka.stream.Materializer;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 
+import static akka.util.ByteString.emptyByteString;
+
 public class HttpClientMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         final ActorSystem system = ActorSystem.create();
 
         final CompletionStage<HttpResponse> responseFuture =
-                Http.get(system)
-                        .singleRequest(HttpRequest.create("https://integration-api-iot.yardienergy.com/iot-api/monitoring"));
+                //Http.get(system)
+                //        .singleRequest(HttpRequest.create("https://integration-api-iot.yardienergy.com/iot-api/monitoring"));
+            Http.get(system)
+                        .singleRequest(HttpRequest.POST("https://httpbin.org/post"));
 
         Materializer materializer = ActorMaterializer.create(system);
+        // consume by using future compose
+        /*
         String result = responseFuture
                 .thenCompose(res -> res.entity().toStrict(10000, materializer))
                 .thenApply(s -> s.getData())
                 .thenApply(d -> d.utf8String())
+                .toCompletableFuture()
+                .get();
+        */
+        String result = responseFuture
+                .thenCompose(res -> res.entity().toStrict(10000, materializer))
+                .thenCompose(strict ->
+                                     strict.getDataBytes()
+                                             .runFold(emptyByteString(), (acc, b) -> acc.concat(b), system)
+                                             .thenApply(s -> s.utf8String())
+                )
                 .toCompletableFuture()
                 .get();
 
