6bca96ff885aaf2906ca60bbf8792d7c5889e61b
davidlzs <davidlzs@gmail.com>
2022-10-25 22:00:08 -0600

    Add killswitch to close websocket from server; add shutdownhook to disconnect websockets from server.

--- src/main/java/com/dliu/akka/lab/WebSocketEchoServer.java
+++ src/main/java/com/dliu/akka/lab/WebSocketEchoServer.java
@@ -1,15 +1,23 @@
 package com.dliu.akka.lab;
 
-import java.util.Arrays;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.*;
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import akka.Done;
 import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.PoisonPill;
+import akka.actor.Terminated;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.HttpTerminated;
+import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.model.ws.Message;
@@ -18,30 +26,71 @@ import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
 import akka.japi.Pair;
 import akka.japi.function.Function;
-import akka.stream.ActorAttributes;
-import akka.stream.ActorMaterializer;
-import akka.stream.Materializer;
-import akka.stream.OverflowStrategy;
-import akka.stream.Supervision;
-import akka.stream.javadsl.BidiFlow;
-import akka.stream.javadsl.Flow;
-import akka.stream.javadsl.Sink;
-import akka.stream.javadsl.Source;
-import akka.stream.javadsl.SourceQueueWithComplete;
+import akka.stream.*;
+import akka.stream.javadsl.*;
+import scala.concurrent.Future;
 
 public class WebSocketEchoServer extends AllDirectives {
     ActorSystem system = ActorSystem.create("websocket-server-system");
     Materializer materializer = ActorMaterializer.create(system);
     Http http = Http.get(system);
+    ServerBinding serverBinding;
 
-    public WebSocketEchoServer() {
+    Set<SharedKillSwitch> killSwitches = Collections.synchronizedSet(new HashSet<>());
+
+
+    public WebSocketEchoServer() throws ExecutionException, InterruptedException, TimeoutException {
         Flow<HttpRequest, HttpResponse, ?> handler = createRoute().flow(system, materializer);
-        http.bindAndHandle(handler, ConnectHttp.toHost("127.0.0.1", 9090), materializer);
+        serverBinding = http.bindAndHandle(handler, ConnectHttp.toHost("127.0.0.1", 9090), materializer).toCompletableFuture().get(3, TimeUnit.SECONDS);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException, TimeoutException {
         WebSocketEchoServer webSocketEchoServer = new WebSocketEchoServer();
+        webSocketEchoServer.registerShutdownHook();
         webSocketEchoServer.runDoubleFlow();
+        webSocketEchoServer.readCommand();
+    }
+
+    private void registerShutdownHook() {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            disconnect();
+//            system.terminate();
+        }));
+    }
+
+    private void readCommand() {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("enter an command");
+        loop: while (true) {
+            String cmd = keyboard.nextLine();
+            switch (cmd) {
+                case "quit" :
+                    terminate();
+                    break loop;
+                case "disconnect":
+                    disconnect();
+                    break;
+            }
+        }
+    }
+
+    private void disconnect() {
+        System.out.println("disconnecting " + killSwitches.size());
+        killSwitches.forEach(SharedKillSwitch::shutdown);
+    }
+
+    private  void terminate() {
+
+        // once ready to terminate the server, invoke terminate:
+        CompletionStage<HttpTerminated> onceAllConnectionsTerminated =
+                serverBinding.terminate(Duration.ofSeconds(3));
+
+// once all connections are terminated,
+        onceAllConnectionsTerminated.toCompletableFuture().
+                thenAccept(terminated -> {
+                    System.out.println("terminating...");
+                    system.terminate();
+                });
     }
 
     private Route createRoute() {
@@ -126,12 +175,15 @@ public class WebSocketEchoServer extends AllDirectives {
 
     // codec flow - reverse
     private Flow<Message, Message, NotUsed> stackedWebSocketFlow() {
+        SharedKillSwitch killSwitch = new SharedKillSwitch("killSwitch");
+        killSwitches.add(killSwitch);
         BidiFlow<String, String, String, String, NotUsed> codec = BidiFlow.fromFunctions(this::reverse, s -> s);
         BidiFlow<Message, String, String, Message, NotUsed> messageStringConverter = BidiFlow.fromFlows(messageToStringFlow(), stringToMessageFlow());
         return messageStringConverter.join(codec.join(Flow.of(String.class)))
                 .watchTermination((mat, termination) -> {
                     System.out.println("connected - add watchTermination");
                     termination.whenComplete((done, throwable) -> {
+                        killSwitches.remove(killSwitch);
                         if (throwable != null) {
                             System.err.println("disconnected with exception: " + throwable);
                         } else {
@@ -139,7 +191,7 @@ public class WebSocketEchoServer extends AllDirectives {
                         }
                     });
                     return mat;
-                });
+                }).viaMat(killSwitch.flow(), Keep.left());
     }
 
     private String reverse(String text) {
